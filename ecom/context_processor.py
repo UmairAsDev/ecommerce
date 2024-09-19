c34ade9e8496271c62e8a7b84cc652cb19e414bb
UmairAsDev <167744708+UmairAsDev@users.noreply.github.com>
2024-09-19 16:20:34 +0500

    slider done

--- ecom/context_processor.py
+++ ecom/context_processor.py
@@ -1,10 +1,14 @@
 from ecom.models import Products, Category, Vendor, CartOrder, CartOrderItems, Wishlist, ProductImages, ProductReview, Address
-
+from django.db.models import Count, Min, Max
 
 def default(request):
     
     categories = Category.objects.all()
     vendors = Vendor.objects.all()
+    
+    # Min Max Price range
+    
+    min_max_price = Products.objects.aggregate(Min("price"), Max("price"))
     try:
         address = Address.objects.get(user=request.user)
     except:
@@ -13,4 +17,5 @@ def default(request):
         "categories": categories,
         "address": address,
         "vendors": vendors,
+        "min_max_price": min_max_price,
     }
