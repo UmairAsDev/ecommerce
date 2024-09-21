3a84e57450c7d227d0bd6b887da795dd6f648ded
UmairAsDev <167744708+UmairAsDev@users.noreply.github.com>
2024-09-21 16:50:32 +0500

    cart template

--- ecom/views.py
+++ ecom/views.py
@@ -215,4 +215,10 @@ def add_to_cart(request):
 
 
 def cart_view(request):
-    return render(request, "ecom/cart.html")
+    cart_total_amount = 0
+    if 'cart_data_obj' in request.session:
+        for p_id, item in request.session['cart_data_obj'].items():
+            cart_total_amount += int(item['qty']) * float(item['price'])
+        return render(request, "ecom/cart.html", {'cart_data': request.session['cart_data_obj'], 'totalcartitems' : len(request.session['cart_data_obj']),'cart_total_amount':cart_total_amount})
+    else:
+        return render(request, "ecom/cart.html", {'cart_data': request.session['cart_data_obj'], 'totalcartitems' : len(request.session['cart_data_obj']),'cart_total_amount':cart_total_amount})
